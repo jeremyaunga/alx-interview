057f56a984b387c82b5fcffa993c30b7d56d10c5
jeremyaunga <aungajeremiah1738@gmail.com>
2024-07-16 12:31:38 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,60 +1,50 @@
 #!/usr/bin/python3
+"""
+log parsing
+"""
+
 import sys
-import signal
+import re
 
-# Initialize metrics
-total_size = 0
-status_counts = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
 
-line_count = 0
+def output(log: dict) -> None:
+    """
+    helper function to display stats
+    """
+    print("File size: {}".format(log["file_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
 
-def print_stats():
-    """Print the current statistics."""
-    print(f"File size: {total_size}")
-    for status_code in sorted(status_counts.keys()):
-        if status_counts[status_code] > 0:
-            print(f"{status_code}: {status_counts[status_code]}")
 
-def signal_handler(sig, frame):
-    """Handle the CTRL+C signal to print stats."""
-    print_stats()
-    sys.exit(0)
+if __name__ == "__main__":
+    regex = re.compile(
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
 
-# Register the signal handler
-signal.signal(signal.SIGINT, signal_handler)
+    line_count = 0
+    log = {}
+    log["file_size"] = 0
+    log["code_frequency"] = {
+        str(code): 0 for code in [
+            200, 301, 400, 401, 403, 404, 405, 500]}
 
-try:
-    for line in sys.stdin:
-        # Check if the line matches the expected format
-        parts = line.split()
-        if len(parts) != 9:
-            continue
-        ip, _, _, date, _, request, _, status_code, file_size = parts
-        if request != '"GET /projects/260 HTTP/1.1"':
-            continue
-        if not status_code.isdigit() or not file_size.isdigit():
-            continue
+    try:
+        for line in sys.stdin:
+            line = line.strip()
+            match = regex.fullmatch(line)
+            if (match):
+                line_count += 1
+                code = match.group(1)
+                file_size = int(match.group(2))
 
-        # Update metrics
-        total_size += int(file_size)
-        if status_code in status_counts:
-            status_counts[status_code] += 1
+                # File size
+                log["file_size"] += file_size
 
-        line_count += 1
+                # status code
+                if (code.isdecimal()):
+                    log["code_frequency"][code] += 1
 
-        # Print stats after every 10 lines
-        if line_count % 10 == 0:
-            print_stats()
-
-except KeyboardInterrupt:
-    print_stats()
-    sys.exit(0)
+                if (line_count % 10 == 0):
+                    output(log)
+    finally:
+        output(log)
